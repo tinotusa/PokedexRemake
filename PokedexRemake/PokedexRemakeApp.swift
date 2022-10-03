456982cad65027edc80e5983aa0528d21bed02fe
Tino Tusaumwe <tusatino@gmail.com>
2022-10-03 17:56:35 +1000

    Initial commit

---
+++ PokedexRemake/PokedexRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexRemakeApp.swift
+//  PokedexRemake
+//
+//  Created by Tino on 3/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

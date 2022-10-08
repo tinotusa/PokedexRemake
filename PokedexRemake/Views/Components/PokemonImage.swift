8c5fb30a23d224444baffe7e7bdd650b9bf227d0
Tino Tusaumwe <tusatino@gmail.com>
2022-10-08 18:59:53 +1000

    Add pokemon image view

---
+++ PokedexRemake/Views/Components/PokemonImage.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonImage.swift
+//  PokedexRemake
+//
+//  Created by Tino on 8/10/2022.
+//
+
+import SwiftUI
+
+struct PokemonImage: View {
+    let url: URL?
+    let imageSize: Double
+    
+    var body: some View {
+        AsyncImage(url: url) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(width: imageSize, height: imageSize)
+    }
+}
+
+struct PokemonImage_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonImage(url: nil, imageSize: 100)
+    }
+}

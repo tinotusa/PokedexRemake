50be62ea471b60abd53f192e57a440c69c46af5b
Tino Tusaumwe <tusatino@gmail.com>
2022-10-09 22:40:05 +1000

    Add MovesTab

---
+++ PokedexRemake/Views/Detail views/Pokemon/Tabs/MovesTab.swift
@@ -0,0 +1,23 @@
+//
+//  MovesTab.swift
+//  PokedexRemake
+//
+//  Created by Tino on 9/10/2022.
+//
+
+import SwiftUI
+import SwiftPokeAPI
+
+struct MovesTab: View {
+    let pokemon: Pokemon
+    
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct MovesTab_Previews: PreviewProvider {
+    static var previews: some View {
+        MovesTab(pokemon: .example)
+    }
+}

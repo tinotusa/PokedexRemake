3e26ace0383590c97fd244842cee4384dc6f35b4
Tino Tusaumwe <tusatino@gmail.com>
2022-10-09 23:05:13 +1000

    Add move card

---
+++ PokedexRemake/Views/Components/MoveCard.swift
@@ -0,0 +1,20 @@
+//
+//  MoveCard.swift
+//  PokedexRemake
+//
+//  Created by Tino on 9/10/2022.
+//
+
+import SwiftUI
+
+struct MoveCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoveCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MoveCard()
+    }
+}

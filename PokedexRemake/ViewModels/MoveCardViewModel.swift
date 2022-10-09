563a8dfeb13cef6ebbac3a1064ed4abc282c9554
Tino Tusaumwe <tusatino@gmail.com>
2022-10-09 23:18:25 +1000

    Update MoveCard

--- PokedexRemake/ViewModels/MoveCardViewModel.swift
+++ PokedexRemake/ViewModels/MoveCardViewModel.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import SwiftPokeAPI
+import os
 
 final class MoveCardViewModel: ObservableObject {
-    
+    @Published private(set) var damageClass: MoveDamageClass!
+    @Published private(set) var type: `Type`!
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "MoveCardViewModel")
+}
+
+extension MoveCardViewModel {
+    func loadData(move: Move) async {
+        logger.debug("Loading data.")
+        do {
+            let id = move.damageClass.url.lastPathComponent
+            let typeID = move.type.url.lastPathComponent
+            
+            async let damageClass = MoveDamageClass(id)
+            async let type = Type(typeID)
+            
+            self.damageClass = try await damageClass
+            self.type = try await type
+            
+            viewLoadingState = .loaded
+            logger.debug("Successfully loaded move data.")
+        } catch {
+            logger.error("Failed to load data.")
+            viewLoadingState = .error(error: error)
+        }
+    }
 }
--- PokedexRemake/Views/Components/MoveCard.swift
+++ PokedexRemake/Views/Components/MoveCard.swift
@@ -6,15 +6,44 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct MoveCard: View {
+    let move: Move
+    @AppStorage(SettingKey.language.rawValue) private var language = "en"
+    @StateObject private var viewModel = MoveCardViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(move: move)
+                }
+        case .loaded:
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(move.localizedName(for: language))
+                    Spacer()
+                    Text(Globals.formattedID(move.id))
+                        .foregroundColor(.gray)
+                }
+                Text(move.effectEntries.first!.shortEffect)
+                    .lineLimit(1)
+                    .foregroundColor(.gray)
+                HStack {
+                    Text(move.damageClass.name!)
+                    Text(move.type.name!)
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct MoveCard_Previews: PreviewProvider {
     static var previews: some View {
-        MoveCard()
+        MoveCard(move: .example)
     }
 }

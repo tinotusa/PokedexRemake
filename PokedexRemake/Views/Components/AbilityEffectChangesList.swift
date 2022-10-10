47846907b4524695bf63e717453de8707fd54cf4
Tino Tusaumwe <tusatino@gmail.com>
2022-10-10 14:27:24 +1000

    Add AbilityEffectChangesList

---
+++ PokedexRemake/Views/Components/AbilityEffectChangesList.swift
@@ -0,0 +1,23 @@
+//
+//  AbilityEffectChangesList.swift
+//  PokedexRemake
+//
+//  Created by Tino on 10/10/2022.
+//
+
+import SwiftUI
+import SwiftPokeAPI
+
+struct AbilityEffectChangesList: View {
+    let effectChanges: [AbilityEffectChange]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AbilityEffectChangesList_Previews: PreviewProvider {
+    static var previews: some View {
+        AbilityEffectChangesList(effectChanges: [])
+    }
+}

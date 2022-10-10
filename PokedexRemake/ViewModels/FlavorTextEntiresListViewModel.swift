83f15898338c7d6c6a62abb1595b047b847836db
Tino Tusaumwe <tusatino@gmail.com>
2022-10-10 13:37:57 +1000

    Add FlavorTextEntriesListViewModel

---
+++ PokedexRemake/ViewModels/FlavorTextEntiresListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorTextEntiresListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 10/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+
+final class FlavorTextEntriesListViewModel: ObservableObject {
+    
+}
+
+extension FlavorTextEntriesListViewModel {
+    
+}

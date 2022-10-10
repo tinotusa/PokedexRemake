73a1680a1fe09883c63d6f2f9f12d9d04421bfb3
Tino Tusaumwe <tusatino@gmail.com>
2022-10-10 12:34:15 +1000

    Add AbilitiesTab

---
+++ PokedexRemake/Views/Detail views/Pokemon/Tabs/AbilitiesTab.swift
@@ -0,0 +1,21 @@
+//
+//  AbilitiesTab.swift
+//  PokedexRemake
+//
+//  Created by Tino on 10/10/2022.
+//
+
+import SwiftUI
+import SwiftPokeAPI
+
+struct AbilitiesTab: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct AbilitiesTab_Preveiws: PreviewProvider {
+    static var previews: some View {
+        AbilitiesTab()
+    }
+}

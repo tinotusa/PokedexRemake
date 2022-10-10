04f2327eff1ca95ce0dd695f006b173807dace77
Tino Tusaumwe <tusatino@gmail.com>
2022-10-10 14:28:40 +1000

    Add AbilityEffectChangesListViewModel

---
+++ PokedexRemake/ViewModels/AbilityEffectChangesListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AbilityEffectChangesListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 10/10/2022.
+//
+
+import Foundation
+
+final class AbilityEffectChangesListViewModel: ObservableObject {
+    
+}
+
+extension AbilityEffectChangesListViewModel {
+    
+}

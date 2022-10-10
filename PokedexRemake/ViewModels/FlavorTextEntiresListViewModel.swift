3ca167bb785b99d924f4200a008d948a31e3c7e2
Tino Tusaumwe <tusatino@gmail.com>
2022-10-10 13:44:17 +1000

    Add loadData(abilityFlavorTexts:)

--- PokedexRemake/ViewModels/FlavorTextEntiresListViewModel.swift
+++ PokedexRemake/ViewModels/FlavorTextEntiresListViewModel.swift
@@ -7,11 +7,26 @@
 
 import Foundation
 import SwiftPokeAPI
+import os
 
 final class FlavorTextEntriesListViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var versionGroups = Set<VersionGroup>()
+    @Published private(set) var versions = Set<Version>()
     
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "FlavorTextEntriesListViewModel")
 }
 
 extension FlavorTextEntriesListViewModel {
-    
+    func loadData(abilityFlavorTexts: [AbilityFlavorText]) async {
+        logger.debug("Loading data.")
+        do {
+            self.versionGroups = try await Globals.getVersionGroups(from: abilityFlavorTexts)
+            viewLoadingState = .loaded
+            logger.debug("Successfully loaded data.")
+        } catch {
+            viewLoadingState = .error(error: error)
+            logger.error("Failed to load data. \(error)")
+        }
+    }
 }

da966e0a5bb45d16e3fbe3aaa3350275f57163ff
Tino Tusaumwe <tusatino@gmail.com>
2022-10-14 13:04:52 +1000

    Add MoveCategoryView

---
+++ PokedexRemake/Views/Categories/Move/MoveCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  MoveCategoryView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 14/10/2022.
+//
+
+import SwiftUI
+
+struct MoveCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoveCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoveCategoryView()
+    }
+}

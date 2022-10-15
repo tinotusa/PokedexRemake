55d2626277857a4009fcefb34f713a7ca6d33157
Tino Tusaumwe <tusatino@gmail.com>
2022-10-15 11:38:36 +1000

    Add ItemsCategoryView

---
+++ PokedexRemake/Views/Categories/ItemsCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemsCategoryView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 15/10/2022.
+//
+
+import SwiftUI
+
+struct ItemsCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemsCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemsCategoryView()
+    }
+}

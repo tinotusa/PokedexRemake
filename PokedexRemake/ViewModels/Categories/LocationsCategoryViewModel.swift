2747acf7a2541a1f88d987a582084b7d78344052
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 12:59:59 +1000

    Add LocationsCategoryViewModel

---
+++ PokedexRemake/ViewModels/Categories/LocationsCategoryViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LocationsCategoryViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import Foundation
+
+final class LocationsCategoryViewModel: ObservableObject {
+    
+}

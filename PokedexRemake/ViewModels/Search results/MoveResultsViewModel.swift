2ff85b0801b473e1e1fc5633e211cea16df855ab
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 15:37:16 +1000

    Add MoveResultsViewModel

---
+++ PokedexRemake/ViewModels/Search results/MoveResultsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MoveResultsViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import Foundation
+
+final class MoveResultsViewModel: ObservableObject {
+    
+}
--- PokedexRemake/Views/Search/Results/MoveResultsView.swift
+++ PokedexRemake/Views/Search/Results/MoveResultsView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct MoveResultsView: View {
+    @ObservedObject var viewMode: MoveResultsViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 

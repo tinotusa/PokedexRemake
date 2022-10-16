6bd6a22fec8209121bf377184a5b9527030af5c7
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 12:05:45 +1000

    Add AbilityCard

---
+++ PokedexRemake/Views/Components/Card/AbilityCard.swift
@@ -0,0 +1,20 @@
+//
+//  AbilityCard.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import SwiftUI
+
+struct AbilityCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AbilityCard_Previews: PreviewProvider {
+    static var previews: some View {
+        AbilityCard()
+    }
+}

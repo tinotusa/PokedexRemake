6e3390d73cd1f5f4fd63f21b1fd203d16905d137
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 14:12:04 +1000

    Add GenerationCard

---
+++ PokedexRemake/Views/Components/Card/GenerationCard.swift
@@ -0,0 +1,20 @@
+//
+//  GenerationCard.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import SwiftUI
+
+struct GenerationCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GenerationCard_Previews: PreviewProvider {
+    static var previews: some View {
+        GenerationCard()
+    }
+}

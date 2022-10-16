394c5fa236bad2b156f8d3de6e9e0975f0d033e8
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 13:57:29 +1000

    Add GenerationsCategoryView

---
+++ PokedexRemake/Views/Categories/GenerationsCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  GenerationsCategoryView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import SwiftUI
+
+struct GenerationsCategoryView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct GenerationsCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenerationsCategoryView()
+    }
+}

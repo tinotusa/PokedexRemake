c0ace545cd3dbe7952af2505b897298a17c374ff
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 13:11:28 +1000

    Add LocationCard

---
+++ PokedexRemake/Views/Components/Card/LocationCard.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCard.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import SwiftUI
+
+struct LocationCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationCard_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationCard()
+    }
+}

f3b0f83f3a7641656f4761e3ac36930af0168862
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 12:59:09 +1000

    Add LocationsCategoryView

---
+++ PokedexRemake/Views/Categories/LocationsCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationsCategoryView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 16/10/2022.
+//
+
+import SwiftUI
+
+struct LocationsCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationsCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsCategoryView()
+    }
+}

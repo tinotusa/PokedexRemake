d201a6a0b786fc9c3f1cf1822d17eb78de58e72c
Tino Tusaumwe <tusatino@gmail.com>
2022-10-16 13:11:02 +1000

    Add LocationsCategoryView UI

--- PokedexRemake/ViewModels/Categories/LocationsCategoryViewModel.swift
+++ PokedexRemake/ViewModels/Categories/LocationsCategoryViewModel.swift
@@ -6,7 +6,52 @@
 //
 
 import Foundation
+import SwiftPokeAPI
+import os
 
 final class LocationsCategoryViewModel: ObservableObject {
+    @Published private(set) var locations = Set<Location>()
+    @Published private(set) var hasNextPage = true
+    @Published private var nextPageURL: URL? {
+        didSet {
+            if nextPageURL == nil { hasNextPage = false }
+        }
+    }
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
     
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "LocationsCategoryViewModel")
+}
+
+extension LocationsCategoryViewModel {
+    func sortedLocations() -> [Location] {
+        self.locations.sorted()
+    }
+    
+    @MainActor
+    func loadData() async {
+        do {
+            let resource = try await Resource<Location>(limit: 20)
+            self.locations = resource.items
+            self.nextPageURL = resource.next
+            viewLoadingState = .loaded
+        } catch  {
+            logger.error("Failed to load data. \(error)")
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    @MainActor
+    func loadNextPage() async {
+        guard let nextPageURL else {
+            logger.error("Failed to load next page. nextPageURL is nil.")
+            return
+        }
+        do {
+            let resource = try await Resource<Location>(nextPageURL)
+            self.locations.formUnion(resource.items)
+            self.nextPageURL = resource.next
+        } catch {
+            logger.error("Failed to load next page url: \(nextPageURL). \(error)")
+        }
+    }
 }
--- PokedexRemake/Views/Categories/LocationsCategoryView.swift
+++ PokedexRemake/Views/Categories/LocationsCategoryView.swift
@@ -6,15 +6,41 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct LocationsCategoryView: View {
+    @ObservedObject var viewModel: LocationsCategoryViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData()
+                }
+        case .loaded:
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.sortedLocations()) { location in
+                        Text(location.name)
+                    }
+                    if viewModel.hasNextPage {
+                        ProgressView()
+                            .task {
+                                await viewModel.loadNextPage()
+                            }
+                    }
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
+        
     }
 }
 
 struct LocationsCategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationsCategoryView()
+        LocationsCategoryView(viewModel: LocationsCategoryViewModel())
     }
 }

65631beff6953a47c7cccec8acb7c2e58f899e2b
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 19:08:11 +1000

    Add load and save to disk functionality

--- PokedexRemake/ViewModels/Search results/ItemResultsViewModel.swift
+++ PokedexRemake/ViewModels/Search results/ItemResultsViewModel.swift
@@ -10,7 +10,11 @@ import SwiftPokeAPI
 import os
 
 final class ItemResultsViewModel: ObservableObject {
-    @Published private(set) var items = [Item]()
+    @Published private(set) var items = [Item]() {
+        didSet {
+            saveHistoryToDisk()
+        }
+    }
     @Published private(set) var hasNextPage = true
     @Published private var nextPageURL: URL? {
         didSet {
@@ -18,17 +22,39 @@ final class ItemResultsViewModel: ObservableObject {
         }
     }
     @Published private(set) var viewLoadingState = ViewLoadingState.loading
-    
+    private let fileIOManager = FileIOManager()
+    static let saveFilename = "itemResults"
     private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "ItemResultsViewModel")
 }
 
 extension ItemResultsViewModel {
+    @MainActor
     func loadData() {
-        viewLoadingState = .loaded
+        logger.debug("Loading data.")
+        do {
+            self.items = try fileIOManager.load([Item].self, documentName: Self.saveFilename)
+            viewLoadingState = .loaded
+            logger.debug("Successfully loaded data from disk.")
+        } catch CocoaError.fileReadNoSuchFile {
+            logger.debug("The file \(Self.saveFilename) has not been created yet.")
+            viewLoadingState = .loaded
+        } catch {
+            logger.error("Failed to load data from disk. \(error)")
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    func saveHistoryToDisk() {
+        do {
+            try fileIOManager.write(self.items, documentName: Self.saveFilename)
+        } catch {
+            logger.error("Failed to write search history to disk. \(error)")
+        }
     }
     
     /// Searches for an Item with the given name.
     /// - parameter name: The name to search for an item with.
+    @MainActor
     func search(_ name: String) async {
         do {
             let item = try await Item(name)

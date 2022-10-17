7cf5cb68891c9d7853829bd0b155688533ba58ba
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 18:58:28 +1000

    Add item search(_:) function

--- PokedexRemake/ViewModels/Search results/ItemResultsViewModel.swift
+++ PokedexRemake/ViewModels/Search results/ItemResultsViewModel.swift
@@ -6,4 +6,38 @@
 //
 
 import Foundation
+import SwiftPokeAPI
+import os
 
+final class ItemResultsViewModel: ObservableObject {
+    @Published private(set) var items = [Item]()
+    @Published private(set) var hasNextPage = true
+    @Published private var nextPageURL: URL? {
+        didSet {
+            if nextPageURL == nil { hasNextPage = false }
+        }
+    }
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "ItemResultsViewModel")
+}
+
+extension ItemResultsViewModel {
+    func loadData() {
+        viewLoadingState = .loaded
+    }
+    
+    /// Searches for an Item with the given name.
+    /// - parameter name: The name to search for an item with.
+    func search(_ name: String) async {
+        do {
+            let item = try await Item(name)
+            let moved = items.moveToTop(item)
+            if !moved {
+                self.items.insert(item, at: 0)
+            }
+        } catch {
+            logger.error("Failed to find item with name: \(name). \(error)")
+        }
+    }
+}

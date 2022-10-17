f48b7821751dd416fbf8aac7e54f3a2137f05558
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 19:47:09 +1000

    Add AbilityResultsView

---
+++ PokedexRemake/Views/Search/Results/AbilityResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  AbilityResultsView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 17/10/2022.
+//
+
+import SwiftUI
+
+struct AbilityResultsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AbilityResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AbilityResultsView()
+    }
+}

1e7e4134a40a11e3649d06a4a6ab271418070aa6
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 19:09:56 +1000

    Add EmptySearchHistoryView

---
+++ PokedexRemake/Views/Components/EmptySearchHistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptySearchHistoryView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 17/10/2022.
+//
+
+import SwiftUI
+
+struct EmptySearchHistoryView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct EmptySearchHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptySearchHistoryView()
+    }
+}

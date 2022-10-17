3c3cee1e0f972c56a74341e2b9d49bcb3937ecbd
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 19:40:27 +1000

    Add ItemResultsView UI

--- PokedexRemake/Views/Search/Results/ItemResultsView.swift
+++ PokedexRemake/Views/Search/Results/ItemResultsView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct ItemResultsView: View {
+    @ObservedObject var viewModel: ItemResultsViewModel
+    
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    viewModel.loadData()
+                }
+        case .loaded:
+            if viewModel.items.isEmpty {
+                if viewModel.isLoading {
+                    ProgressView()
+                }
+                EmptySearchHistoryView(text: "Search for some items.", errorMessage: viewModel.errorMessage)
+            } else {
+                ScrollView {
+                    LazyVStack {
+                        if viewModel.isLoading {
+                            ProgressView()
+                        }
+                        
+                        SearchErrorView(text: viewModel.errorMessage)
+                        
+                        ForEach(viewModel.items) { item in
+                            ItemCard(item: item)
+                                .simultaneousGesture(
+                                    TapGesture()
+                                        .onEnded {
+                                            viewModel.moveToTop(item)
+                                        }
+                                )
+                        }
+                    }
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct ItemResultsView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemResultsView()
+        ItemResultsView(viewModel: ItemResultsViewModel())
     }
 }

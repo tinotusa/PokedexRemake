d7bf0ce1c0c4a44990b79d935bb4b5ff1c6b52ea
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 18:23:18 +1000

    Add ItemsResultsView

---
+++ PokedexRemake/Views/Search/Results/ItemResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemResultsView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 17/10/2022.
+//
+
+import SwiftUI
+
+struct ItemResultsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ItemResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemResultsView()
+    }
+}

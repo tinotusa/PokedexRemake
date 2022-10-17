37b95d0498a463a03d379ca740583b64d35323be
Tino Tusaumwe <tusatino@gmail.com>
2022-10-17 19:12:11 +1000

    Add UI for EmptySearchHistoryView

--- PokedexRemake/Views/Components/EmptySearchHistoryView.swift
+++ PokedexRemake/Views/Components/EmptySearchHistoryView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct EmptySearchHistoryView: View {
+    let text: LocalizedStringKey
+    var errorMessage: String?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            Text(text)
+                .foregroundColor(.gray)
+                .bodyStyle()
+                .multilineTextAlignment(.center)
+            
+            SearchErrorView(text: errorMessage)
+            
+            Spacer()
+        }
     }
 }
 
 struct EmptySearchHistoryView_Previews: PreviewProvider {
     static var previews: some View {
-        EmptySearchHistoryView()
+        EmptySearchHistoryView(text: "Search for something.")
     }
 }

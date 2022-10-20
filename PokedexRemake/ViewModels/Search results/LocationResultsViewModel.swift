fa5a3812c6ee2a45cecf684593ab3ec8934cc52e
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 12:51:57 +1000

    Add LocationResultsViewModel

---
+++ PokedexRemake/ViewModels/Search results/LocationResultsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LocationResultsViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 20/10/2022.
+//
+
+import Foundation
+
+final class LocationResultsViewModel: ObservableObject {
+    
+}

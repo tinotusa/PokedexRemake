fbec8bb79c8498c96e69e714a467cb41a79054d0
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 15:24:06 +1000

    Add PokemonListViewModel

---
+++ PokedexRemake/ViewModels/PokemonListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 20/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class PokemonListViewModel: ObservableObject {
+    
+}

49350292e4afb45da31d5cff759cba560a549f5c
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 12:51:04 +1000

    Add LocationResultsView

---
+++ PokedexRemake/Views/Search/Results/LocationResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationResultsView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 20/10/2022.
+//
+
+import SwiftUI
+
+struct LocationResultsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LocationResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationResultsView()
+    }
+}

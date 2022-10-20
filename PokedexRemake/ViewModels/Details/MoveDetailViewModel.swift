a8a2d659e1611d33a8edf0d1822f624634579947
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 13:30:54 +1000

    Add MoveDetailViewModel

---
+++ PokedexRemake/ViewModels/Details/MoveDetailViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MoveDetailViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 20/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class MoveDetailViewModel: ObservableObject {
+    
+}

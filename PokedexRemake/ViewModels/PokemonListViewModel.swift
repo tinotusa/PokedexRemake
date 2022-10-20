e60c9c2aa85ca59d76c34ead5d14fcea035c8246
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 15:36:51 +1000

    Add PokemonListView and view model

--- PokedexRemake/ViewModels/PokemonListViewModel.swift
+++ PokedexRemake/ViewModels/PokemonListViewModel.swift
@@ -10,5 +10,79 @@ import SwiftPokeAPI
 import os
 
 final class PokemonListViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private var urls = [URL]()
+    @Published private(set) var pokemon = [Pokemon]()
+    @Published private(set) var hasNextPage = true
+    private var page = 0 {
+        didSet {
+            offset = page * limit
+        }
+    }
+    private var limit = 20
+    private var offset = 0
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "PokemonListViewModel")
+}
+
+extension PokemonListViewModel {
+    func loadData(urls: [URL]) async {
+        self.urls = urls
+        do {
+            let pokemon = try await getPokemon(urls: urls)
+            if pokemon.count < limit {
+                hasNextPage = false
+            }
+            self.pokemon = pokemon
+            viewLoadingState = .loaded
+        } catch {
+            viewLoadingState = .error(error: error)
+        }
+    }
+
+    func loadNextPage() async {
+        logger.debug("Loading next page.")
+        do {
+            let newPokemon = try await getNextPokemonPage()
+            if newPokemon.count < limit {
+                hasNextPage = false
+            }
+            self.pokemon.append(contentsOf: newPokemon)
+        } catch {
+            logger.error("Failed to load next pokemon page. \(error)")
+        }
+    }
     
+}
+
+private extension PokemonListViewModel {
+    func getPokemon(urls: [URL]) async throws -> [Pokemon] {
+        try await withThrowingTaskGroup(of: Pokemon.self) { group in
+            for (i, url) in urls.enumerated() where i < limit {
+                group.addTask {
+                    return try await Pokemon(url)
+                }
+            }
+            
+            var pokemonArray = [Pokemon]()
+            for try await pokemon in group {
+                pokemonArray.append(pokemon)
+            }
+            return pokemonArray
+        }
+    }
+    
+    func getNextPokemonPage() async throws -> [Pokemon] {
+        try await withThrowingTaskGroup(of: Pokemon.self) { group in
+            for (i, url) in urls.enumerated() where i > offset && i < offset + limit {
+                group.addTask {
+                    return try await Pokemon(url)
+                }
+            }
+            var pokemonArray = [Pokemon]()
+            for try await pokemon in group {
+                pokemonArray.append(pokemon)
+            }
+            return pokemonArray
+        }
+    }
 }
--- PokedexRemake/Views/PokemonListView.swift
+++ PokedexRemake/Views/PokemonListView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct PokemonListView: View {
     let pokemoURLs: [URL]
+    @ObservedObject var viewModel: PokemonListViewModel
     
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(urls: pokemoURLs)
+                }
+        case .loaded:
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.pokemon) { pokemon in
+                        PokemonResultRow(pokemon: pokemon)
+                    }
+                    if viewModel.hasNextPage {
+                        ProgressView()
+                            .task {
+                                await viewModel.loadNextPage()
+                            }
+                    }
+                }
+            }
+            .bodyStyle()
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct PokemonListView_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonListView(pokemoURLs: [])
+        PokemonListView(pokemoURLs: [], viewModel: PokemonListViewModel())
     }
 }

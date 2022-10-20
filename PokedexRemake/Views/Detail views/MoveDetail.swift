46483490c93aac75c127a8174b8b7b663fd75933
Tino Tusaumwe <tusatino@gmail.com>
2022-10-20 13:27:23 +1000

    Add MoveDetail

---
+++ PokedexRemake/Views/Detail views/MoveDetail.swift
@@ -0,0 +1,23 @@
+//
+//  MoveDetail.swift
+//  PokedexRemake
+//
+//  Created by Tino on 20/10/2022.
+//
+
+import SwiftUI
+import SwiftPokeAPI
+
+struct MoveDetail: View {
+    let move: Move
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MoveDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MoveDetail(move: .example)
+    }
+}

206a367b2b1063fba475f113429a5e5647bd51a4
Tino Tusaumwe <tusatino@gmail.com>
2022-10-21 12:23:35 +1000

    Add EffectEntriesListViewModel

---
+++ PokedexRemake/ViewModels/EffectEntriesListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  EffectEntriesListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 21/10/2022.
+//
+
+import Foundation
+
+final class EffectEntriesListViewModel: ObservableObject {
+    
+}

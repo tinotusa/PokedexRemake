0d1edb164417e084741c79197f2b94e51a9bcca9
Tino Tusaumwe <tusatino@gmail.com>
2022-10-21 13:09:13 +1000

    Add EffectChangesListView

---
+++ PokedexRemake/Views/EffectChangesListView.swift
@@ -0,0 +1,20 @@
+//
+//  EffectChangesListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 21/10/2022.
+//
+
+import SwiftUI
+
+struct EffectChangesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EffectChangesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EffectChangesListView()
+    }
+}

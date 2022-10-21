a4070183f93d36f54e68810c93e75e464feeac61
Tino Tusaumwe <tusatino@gmail.com>
2022-10-21 13:07:13 +1000

    Add DetailListView UI

--- PokedexRemake/Views/DetailListView.swift
+++ PokedexRemake/Views/DetailListView.swift
@@ -7,14 +7,37 @@
 
 import SwiftUI
 
-struct DetailListView: View {
+struct DetailListView<Content: View>: View {
+    let title: String
+    let id: Int
+    let description: LocalizedStringKey
+    let content: () -> Content
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                HeaderBar(title: title, id: id)
+                Text(description)
+                content()
+            }
+            .padding()
+        }
+        .bodyStyle()
     }
 }
 
 struct DetailListView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailListView()
+        DetailListView(
+            title: "some title",
+            id: 999,
+            description: "some description goes here"
+        ) {
+            VStack(alignment: .leading) {
+                ForEach(0 ..< 10) { index in
+                    Text("This is row: \(index + 1)")
+                }
+            }
+        }
     }
 }

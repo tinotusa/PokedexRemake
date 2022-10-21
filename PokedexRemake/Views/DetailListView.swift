4d179b5d51a5b5bbb6e725203062db6ad3a6e5d1
Tino Tusaumwe <tusatino@gmail.com>
2022-10-21 12:53:22 +1000

    Add DetailListView

---
+++ PokedexRemake/Views/DetailListView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 21/10/2022.
+//
+
+import SwiftUI
+
+struct DetailListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailListView()
+    }
+}

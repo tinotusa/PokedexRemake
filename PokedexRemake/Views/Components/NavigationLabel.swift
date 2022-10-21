002934eea0bd619ed796696c273bec2e463705ea
Tino Tusaumwe <tusatino@gmail.com>
2022-10-21 12:22:51 +1000

    Add effect entries view

---
+++ PokedexRemake/Views/Components/NavigationLabel.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationLabel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 21/10/2022.
+//
+
+import SwiftUI
+
+struct NavigationLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationLabel()
+    }
+}
---
+++ PokedexRemake/Views/EffectEntriesListView.swift
@@ -0,0 +1,20 @@
+//
+//  EffectEntriesListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 21/10/2022.
+//
+
+import SwiftUI
+
+struct EffectEntriesListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct EffectEntriesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        EffectEntriesListView()
+    }
+}

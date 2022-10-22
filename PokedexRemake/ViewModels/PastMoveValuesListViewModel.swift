7e38bddc71c09dc1d99e2ce40f4d84313b70277c
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 16:20:08 +1000

    Add PastMoveValuesList UI

--- PokedexRemake/ViewModels/PastMoveValuesListViewModel.swift
+++ PokedexRemake/ViewModels/PastMoveValuesListViewModel.swift
@@ -10,5 +10,27 @@ import SwiftPokeAPI
 import os
 
 final class PastMoveValuesListViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var types = Set<`Type`>()
+    @Published private(set) var versionGroups = Set<VersionGroup>()
+    @Published private(set) var versions = Set<Version>()
+    
     private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "PastMoveValuesListViewModel")
+}
+
+extension PastMoveValuesListViewModel {
+    @MainActor
+    func loadData(pastValues: [PastMoveStatValues]) async {
+        logger.debug("Loading data.")
+        do {
+            self.types = try await Globals.getTypes(urls: pastValues.compactMap { $0.type?.url })
+            self.versionGroups = try await Globals.getVersionGroups(from: pastValues.compactMap { $0.versionGroup.url })
+            self.versions = try await Globals.getVersions(from: versionGroups)
+            viewLoadingState = .loaded
+            logger.debug("Successfully loaded data.")
+        } catch {
+            logger.error("Failed to load data. \(error)")
+            viewLoadingState = .error(error: error)
+        }
+    }
 }
--- PokedexRemake/Views/PastMoveValuesListView.swift
+++ PokedexRemake/Views/PastMoveValuesListView.swift
@@ -6,15 +6,41 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct PastMoveValuesListView: View {
+    let title: String
+    let id: Int
+    let description: LocalizedStringKey
+    let pastValues: [PastMoveStatValues]
+    @StateObject private var viewModel = PastMoveValuesListViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(pastValues: pastValues)
+                }
+        case .loaded:
+            DetailListView(title: title, id: id, description: description) {
+                ForEach(pastValues, id: \.self) { pastValue in
+                    PastMoveValueView(pastValue: pastValue)
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct PastMoveValuesListView_Previews: PreviewProvider {
     static var previews: some View {
-        PastMoveValuesListView()
+        PastMoveValuesListView(
+            title: "some title",
+            id: 123,
+            description: "some description goes here.",
+            pastValues: Move.flashExample.pastValues
+        )
     }
 }

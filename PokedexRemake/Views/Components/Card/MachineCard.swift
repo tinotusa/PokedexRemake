29071a9ec3bd4418412f5aae71683ef55fa5862a
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 11:42:40 +1000

    Add MachineCard

---
+++ PokedexRemake/Views/Components/Card/MachineCard.swift
@@ -0,0 +1,20 @@
+//
+//  MachineCard.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import SwiftUI
+
+struct MachineCard: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MachineCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MachineCard()
+    }
+}

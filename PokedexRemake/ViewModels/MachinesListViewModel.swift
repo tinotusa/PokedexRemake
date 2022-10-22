aba44e452ed4313fbcfd129f4f4e0102e8473c54
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 10:55:04 +1000

    Add MachinesListViewModel

---
+++ PokedexRemake/ViewModels/MachinesListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MachinesListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class MachinesListViewModel: ObservableObject {
+    
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "MachinesListViewModel")
+}

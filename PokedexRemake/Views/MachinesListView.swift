6b50ee04b34c61c662a2917572ee1497233cba1e
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 10:51:42 +1000

    Add MachinesListView

---
+++ PokedexRemake/Views/MachinesListView.swift
@@ -0,0 +1,20 @@
+//
+//  MachinesListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import SwiftUI
+
+struct MachinesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MachinesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MachinesListView()
+    }
+}

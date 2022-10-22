253f3559db955d028c43a818386ffa14b54b7a27
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 17:01:47 +1000

    Update MoveStatChangeListView UI

--- PokedexRemake/Views/MoveStatChangeListView.swift
+++ PokedexRemake/Views/MoveStatChangeListView.swift
@@ -6,15 +6,34 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct MoveStatChangeListView: View {
+    let title: String
+    let id: Int
+    let description: LocalizedStringKey
+    let statChanges: [MoveStatChange]
+    
     var body: some View {
-        Text("Hello, World!")
+        DetailListView(
+            title: title,
+            id: id,
+            description: description
+        ) {
+            ForEach(statChanges, id: \.self) { statChange in
+                MoveStatChangeView(statChange: statChange)
+            }
+        }
     }
 }
 
 struct MoveStatChangeListView_Previews: PreviewProvider {
     static var previews: some View {
-        MoveStatChangeListView()
+        MoveStatChangeListView(
+            title: "some title",
+            id: 123,
+            description: "description here",
+            statChanges: Move.example.statChanges
+        )
     }
 }

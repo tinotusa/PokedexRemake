8ea74c65dc6dbd9e3a6b9064d95405f4a7e7d5df
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 12:48:18 +1000

    Add PastMoveValuesListView

---
+++ PokedexRemake/Views/PastMoveValuesListView.swift
@@ -0,0 +1,20 @@
+//
+//  PastMoveValuesListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import SwiftUI
+
+struct PastMoveValuesListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PastMoveValuesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PastMoveValuesListView()
+    }
+}

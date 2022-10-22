c99e6edc6af25defc19b83ed315b9ad4bc34d985
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 12:51:28 +1000

    Add PastMoveValuesListViewModel

---
+++ PokedexRemake/ViewModels/PastMoveValuesListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PastMoveValuesListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class PastMoveValuesListViewModel: ObservableObject {
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "PastMoveValuesListViewModel")
+}

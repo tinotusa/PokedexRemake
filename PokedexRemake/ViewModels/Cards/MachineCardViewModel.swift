89202167eff04ae4cf13e3aff368eab467cd160a
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 11:51:29 +1000

    Add MachineCardViewModel

---
+++ PokedexRemake/ViewModels/Cards/MachineCardViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MachineCardViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class MachineCardViewModel: ObservableObject {
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "MachineCardViewModel")
+}

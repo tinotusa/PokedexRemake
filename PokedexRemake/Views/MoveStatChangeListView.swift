576f461bcb41aedc5368e302cd7e3bdafd633daa
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 16:55:38 +1000

    Add MoveStatChangeListView

---
+++ PokedexRemake/Views/MoveStatChangeListView.swift
@@ -0,0 +1,20 @@
+//
+//  MoveStatChangeListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import SwiftUI
+
+struct MoveStatChangeListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MoveStatChangeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoveStatChangeListView()
+    }
+}

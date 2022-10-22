04102635710366c59752e2b2f341c8e2c23da755
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 11:17:04 +1000

    Add MachineListView UI

--- PokedexRemake/Views/MachinesListView.swift
+++ PokedexRemake/Views/MachinesListView.swift
@@ -6,15 +6,49 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct MachinesListView: View {
+    let title: String
+    let id: Int
+    let description: LocalizedStringKey
+    let machineURLs: [URL]
+    @StateObject private var viewModel = MachinesListViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(urls: machineURLs)
+                }
+        case .loaded:
+            DetailListView(
+                title: title,
+                id: id,
+                description: description
+            ) {
+                LazyVStack {
+                    ForEach(viewModel.sortedMachines()) { machine in
+                        //                    MachineCard(machine: machine)
+                        Text("\(machine.item.name!)")
+                            .padding()
+                    }
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct MachinesListView_Previews: PreviewProvider {
     static var previews: some View {
-        MachinesListView()
+        MachinesListView(
+            title: "some title",
+            id: 123,
+            description: "a description",
+            machineURLs: Move.example.machines.map { $0.machine.url}
+        )
     }
 }

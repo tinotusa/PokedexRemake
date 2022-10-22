714ca9c6f8d3246ad9f3639ac8444df508632aa9
Tino Tusaumwe <tusatino@gmail.com>
2022-10-22 16:19:03 +1000

    Rename PastMoveValue views both list and singluar

---
+++ PokedexRemake/ViewModels/PastMoveValueViewModel.swift
@@ -0,0 +1,86 @@
+//
+//  PastMoveValueViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import SwiftUI
+import os
+
+final class PastMoveValueViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var type: `Type`?
+    @Published private var versionGroup: VersionGroup?
+    @Published private var versions = Set<Version>()
+    @Published private(set) var pastValues = [PastValueKey: String]()
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "PastMoveListViewModel")
+    
+    enum PastValueKey: String, CaseIterable, Identifiable {
+        case type
+        case accuracy
+        case effectChance = "effect chance"
+        case power
+        case pp
+        case effectEntries = "effect entries"
+        
+        var id: Self { self }
+        
+        var title: LocalizedStringKey {
+            LocalizedStringKey(self.rawValue.localizedCapitalized)
+        }
+    }
+}
+
+extension PastMoveValueViewModel {
+    func sortedVersion() -> [Version] {
+        self.versions.sorted()
+    }
+    
+    @MainActor
+    func loadData(pastValue: PastMoveStatValues) async {
+        logger.debug("Loading data.")
+        do {
+            if let typeURL = pastValue.type?.url {
+                self.type = try await `Type`(typeURL)
+            }
+            self.versionGroup = try await VersionGroup(pastValue.versionGroup.url)
+            if let versionGroup {
+                self.versions = try await Globals.getVersions(from: [versionGroup])
+            }
+            viewLoadingState = .loaded
+            self.pastValues = getPastValues(pastValue: pastValue)
+            logger.debug("Successfully loaded data.")
+        } catch {
+            logger.error("Failed to load data. \(error)")
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    func getPastValues(pastValue: PastMoveStatValues) -> [PastValueKey: String] {
+        logger.debug("Getting past value keys.")
+        var values = [PastValueKey: String]()
+        if let type {
+            values[.type] = type.name
+        }
+        if let accuracy = pastValue.accuracy {
+            values[.accuracy] = accuracy.formatted(.percent)
+        }
+        if let effectChance = pastValue.effectChance {
+            values[.effectChance] = effectChance.formatted(.percent)
+        }
+        if let power = pastValue.power {
+            values[.power] = "\(power)"
+        }
+        if let pp = pastValue.pp {
+            values[.pp] = "\(pp)"
+        }
+        if !pastValue.effectEntries.isEmpty {
+            values[.effectEntries] = "\(pastValue.effectEntries.count)"
+        }
+        logger.debug("Successfully got past value keys.")
+        return values
+    }
+}
---
+++ PokedexRemake/Views/PastMoveValueView.swift
@@ -0,0 +1,88 @@
+//
+//  PastMoveValueListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 22/10/2022.
+//
+
+import SwiftUI
+import SwiftPokeAPI
+
+struct PastMoveValueView: View {
+    let pastValue: PastMoveStatValues
+    @StateObject private var viewModel = PastMoveValueListViewModel()
+    @AppStorage(SettingsKey.language.rawValue) private var language = SettingsKey.defaultLanguage
+    @StateObject private var effectEntriesListViewModel = EffectEntriesListViewModel()
+    var body: some View {
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(pastValue: pastValue)
+                }
+        case .loaded:
+            VStack(alignment: .leading) {
+                ViewThatFits(in: .horizontal) {
+                    HStack {
+                        versionsList
+                    }
+                    VStack {
+                        versionsList
+                    }
+                }
+                .subtitleStyle()
+                
+                Grid(alignment: .leading) {
+                    ForEach(PastMoveValueListViewModel.PastValueKey.allCases) { pastValueKey in
+                        let value = viewModel.pastValues[pastValueKey, default: "N/A"]
+                        GridRow {
+                            Text(pastValueKey.title)
+                                .foregroundColor(.gray)
+                            switch pastValueKey {
+                            case .type where value != "N/A":
+                                if let type = viewModel.type {
+                                    TypeTag(type: type)
+                                }
+                            case .effectEntries where value != "N/A":
+                                if pastValue.effectEntries.isEmpty {
+                                    Text(value)
+                                } else {
+                                    NavigationLink {
+                                        EffectEntriesListView(
+                                            title: "Past entries",
+                                            id: 0,
+                                            description: "Past entries for this move",
+                                            entries: pastValue.effectEntries, viewModel: effectEntriesListViewModel
+                                        )
+                                    } label: {
+                                        NavigationLabel(title: value)
+                                    }
+                                }
+                            default: Text(value)
+                                    .foregroundColor(value == "N/A" ? .gray : .text)
+                            }
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .bodyStyle()
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
+    }
+}
+
+private extension PastMoveValueView {
+    var versionsList: some View {
+        ForEach(viewModel.sortedVersion()) { version in
+            Text(version.localizedName(for: language))
+        }
+    }
+}
+
+struct PastMoveValueListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PastMoveValueView(pastValue: Move.flashExample.pastValues.first!)
+    }
+}

3a9431117284be5b9b0ef64b191971ca593ebaa3
Tino Tusaumwe <tusatino@gmail.com>
2022-10-23 13:11:01 +1000

    Add pagination functionality

--- PokedexRemake/ViewModels/MovesListViewModel.swift
+++ PokedexRemake/ViewModels/MovesListViewModel.swift
@@ -8,10 +8,29 @@
 import Foundation
 import SwiftPokeAPI
 import os
+
 final class MovesListViewModel: ObservableObject {
     @Published private(set) var pokemonSpecies: PokemonSpecies!
-    @Published private var moves = Set<Move>()
+    @Published private(set) var hasNextPage = true
+    
+    @Published private var moves = Set<Move>() {
+        willSet {
+            page += 1
+            if abs(moves.count - newValue.count) < limit {
+                hasNextPage = false
+            }
+        }
+    }
     @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    
+    private var urls = [URL]()
+    private var page = 0 {
+        didSet {
+            offset = page * limit
+        }
+    }
+    private var offset = 0
+    private let limit = 20
     
     private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "MovesTabViewModel")
 }
@@ -20,12 +39,26 @@ extension MovesListViewModel {
     @MainActor
     func loadData(pokemon: Pokemon) async {
         logger.debug("Loading data.")
+        let urls = pokemon.moves.map { $0.move.url }
+        self.urls = urls
+        
         do {
-            self.moves = try await Globals.getMoves(urls: pokemon.moves.map { $0.move.url })
+            self.moves = try await Globals.getMoves(urls: urls, limit: limit)
             viewLoadingState = .loaded
         } catch {
             logger.error("Failed load data.")
             viewLoadingState = .error(error: error)
+        }
+    }
+    
+    @MainActor
+    func getNextPage() async {
+        do {
+            let moves = try await Globals.getMoves(urls: self.urls, limit: 20, offset: offset)
+            self.moves.formUnion(moves)
+            logger.debug("Successfully got the next page.")
+        } catch {
+            logger.error("Failed to get next page. \(error)")
         }
     }
     

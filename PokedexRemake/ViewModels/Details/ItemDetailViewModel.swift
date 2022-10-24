9ae19704b2b5b45fa652d4b2b50fd63f28e61a80
Tino Tusaumwe <tusatino@gmail.com>
2022-10-24 15:58:41 +1000

    Add ItemDetailViewModel

---
+++ PokedexRemake/ViewModels/Details/ItemDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ItemDetailViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 24/10/2022.
+//
+
+import Foundation
+
+final class ItemDetailViewModel: ObservableObject {
+    
+}

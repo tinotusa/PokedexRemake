485c3cba0a4be752e8b4ff6bcf07446a6ea40097
Tino Tusaumwe <tusatino@gmail.com>
2022-10-24 13:29:53 +1000

    Add ItemDetail

---
+++ PokedexRemake/Views/Detail views/ItemDetail.swift
@@ -0,0 +1,20 @@
+//
+//  ItemDetail.swift
+//  PokedexRemake
+//
+//  Created by Tino on 24/10/2022.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail()
+    }
+}

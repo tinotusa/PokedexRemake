04bfd9b60af6ee748388dc12c690553c22a611bd
Tino Tusaumwe <tusatino@gmail.com>
2022-10-27 19:40:48 +1000

    Add Localizable protocol

---
+++ PokedexRemake/Extensions/Localizable+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Localizable+Extensions.swift
+//  PokedexRemake
+//
+//  Created by Tino on 27/10/2022.
+//
+
+import Foundation
---
+++ PokedexRemake/Models/Localizable.swift
@@ -0,0 +1,13 @@
+//
+//  Localizable.swift
+//  PokedexRemake
+//
+//  Created by Tino on 27/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+
+protocol Localizable {
+    var language: NamedAPIResource { get }
+}

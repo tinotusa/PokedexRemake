848adf7fd5fb1e1a3f1e68c8b961e517c057765f
Tino Tusaumwe <tusatino@gmail.com>
2022-10-30 14:42:36 +1000

    Add AbilityDetailViewModel

---
+++ PokedexRemake/ViewModels/Details/AbilityDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AbilityDetailViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 30/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+
+final class AbilityDetailViewModel {
+    
+}

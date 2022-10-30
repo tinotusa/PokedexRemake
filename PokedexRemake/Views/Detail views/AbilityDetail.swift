6f3d5e3b90a2df8530358adb1b10c2c486d8f007
Tino Tusaumwe <tusatino@gmail.com>
2022-10-30 14:38:20 +1000

    Add AbilityDetail

---
+++ PokedexRemake/Views/Detail views/AbilityDetail.swift
@@ -0,0 +1,20 @@
+//
+//  AbilityDetail.swift
+//  PokedexRemake
+//
+//  Created by Tino on 30/10/2022.
+//
+
+import SwiftUI
+
+struct AbilityDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AbilityDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        AbilityDetail()
+    }
+}

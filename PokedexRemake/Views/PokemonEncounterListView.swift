a7fb1708fd6e9dbe61de8b15d64bcf503bc50c1f
Tino Tusaumwe <tusatino@gmail.com>
2022-10-31 19:44:52 +1000

    Add PokemonEncounterListView

---
+++ PokedexRemake/Views/PokemonEncounterListView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonEncounterListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 31/10/2022.
+//
+
+import SwiftUI
+
+struct PokemonEncounterListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PokemonEncounterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonEncounterListView()
+    }
+}

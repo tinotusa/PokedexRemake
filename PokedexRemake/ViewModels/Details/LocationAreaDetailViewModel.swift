079c43289bc66e1f9f5f9886325738b0520dfdc1
Tino Tusaumwe <tusatino@gmail.com>
2022-10-31 18:42:10 +1000

    Add LocationAreaDetailViewModel

---
+++ PokedexRemake/ViewModels/Details/LocationAreaDetailViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LocationAreaDetailViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 31/10/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class LocationAreaDetailViewModel: ObservableObject {
+    private var logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "LocationAreaDetailViewModel")
+}

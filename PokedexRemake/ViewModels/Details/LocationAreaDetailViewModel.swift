6b39959e8430d4261b58c7809fac6fe2787a0687
Tino Tusaumwe <tusatino@gmail.com>
2022-10-31 19:36:23 +1000

    Add LocationAreaDetail UI

--- PokedexRemake/ViewModels/Details/LocationAreaDetailViewModel.swift
+++ PokedexRemake/ViewModels/Details/LocationAreaDetailViewModel.swift
@@ -10,5 +10,48 @@ import SwiftPokeAPI
 import os
 
 final class LocationAreaDetailViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var encounterVersions = [EncounterVersion]()
+    @Published var showingPokemonList = false
+    
     private var logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "LocationAreaDetailViewModel")
+    
+    struct EncounterVersion: Identifiable {
+        let id = UUID().uuidString
+        let encounterMethod: EncounterMethod
+        let versionDetails: [(rate: Int, version: Version)]
+    }
+}
+
+extension LocationAreaDetailViewModel {
+    func loadData(locationArea: LocationArea) async {
+        do {
+            self.encounterVersions = try await getEncounterMethodVersions(locationArea: locationArea)
+            
+            viewLoadingState = .loaded
+        } catch {
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    func getEncounterMethodVersions(locationArea: LocationArea) async throws -> [EncounterVersion] {
+        try await withThrowingTaskGroup(of: EncounterVersion.self) { group in
+            for method in locationArea.encounterMethodRates {
+                group.addTask {
+                    let encounterMethod = try await EncounterMethod(method.encounterMethod.url)
+                    var versionDetails = [(rate: Int, version: Version)]()
+                    for test in method.versionDetails {
+                        let version = try await Version(test.version.url)
+                        versionDetails.append((rate: test.rate, version: version))
+                    }
+                    return EncounterVersion(encounterMethod: encounterMethod, versionDetails: versionDetails)
+                }
+            }
+            var encounterVersions = [EncounterVersion]()
+            for try await encounterVersion in group {
+                encounterVersions.append(encounterVersion)
+            }
+            return encounterVersions
+        }
+    }
 }
--- PokedexRemake/Views/Detail views/LocationAreaDetail.swift
+++ PokedexRemake/Views/Detail views/LocationAreaDetail.swift
@@ -6,15 +6,71 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct LocationAreaDetail: View {
+    let locationArea: LocationArea
+    @AppStorage(SettingsKey.language.rawValue) private var languageCode = SettingsKey.defaultLanguage
+    @StateObject private var viewModel = LocationAreaDetailViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(locationArea: locationArea)
+                }
+        case .loaded:
+            ScrollView {
+                Grid(alignment: .topLeading, verticalSpacing: 8) {
+                    GridRow {
+                        Text("Name")
+                            .foregroundColor(.gray)
+                        let name = locationArea.localizedName(languageCode: languageCode)
+                        if name.isEmpty {
+                            Text(locationArea.name)
+                        } else {
+                            Text(name)
+                        }
+                    }
+                    GridRow {
+                        Text("Encounter method rates")
+                            .foregroundColor(.gray)
+                        VStack(alignment: .leading) {
+                            ForEach(viewModel.encounterVersions) { encounterVersion in
+                                Text(encounterVersion.encounterMethod.localizedName(languageCode: languageCode))
+                                ForEach(encounterVersion.versionDetails, id: \.version) { details in
+                                    HStack {
+                                        Text(details.rate.formatted(.percent))
+                                        Text(details.version.localizedName(languageCode: languageCode))
+                                    }
+                                }
+                                Divider()
+                            }
+                        }
+                    }
+                    GridRow {
+                        Text("Pokemon encounters")
+                            .foregroundColor(.gray)
+                        NavigationLink {
+                            Text("PokemonEncounter list todo.")
+                        } label: {
+                            NavigationLabel(title: "\(locationArea.pokemonEncounters.count)")
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+            .bodyStyle()
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct LocationAreaDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LocationAreaDetail()
+        LocationAreaDetail(locationArea: .example)
     }
 }

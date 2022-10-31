4606d8d9ed045716ffc34b09f6f90128cae423ba
Tino Tusaumwe <tusatino@gmail.com>
2022-10-31 18:28:56 +1000

    Add LocationAreaDetail

---
+++ PokedexRemake/Views/Detail views/LocationAreaDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LocationAreaDetail.swift
+//  PokedexRemake
+//
+//  Created by Tino on 31/10/2022.
+//
+
+import SwiftUI
+
+struct LocationAreaDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LocationAreaDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationAreaDetail()
+    }
+}

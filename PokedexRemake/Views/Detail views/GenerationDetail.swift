dfef3339d97bf52dd91da7850c1e4629724b6693
Tino Tusaumwe <tusatino@gmail.com>
2022-11-01 20:36:31 +1000

    Add GenerationDetail

---
+++ PokedexRemake/Views/Detail views/GenerationDetail.swift
@@ -0,0 +1,20 @@
+//
+//  GenerationDetail.swift
+//  PokedexRemake
+//
+//  Created by Tino on 1/11/2022.
+//
+
+import SwiftUI
+
+struct GenerationDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct GenerationDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        GenerationDetail()
+    }
+}

dadf9e383b60efb1d6ec7e4be339da2420ad782b
Tino Tusaumwe <tusatino@gmail.com>
2022-11-01 21:29:12 +1000

    Add GenerationDetail UI

--- PokedexRemake/ViewModels/Details/GenerationDetailViewModel.swift
+++ PokedexRemake/ViewModels/Details/GenerationDetailViewModel.swift
@@ -6,7 +6,71 @@
 //
 
 import Foundation
+import SwiftPokeAPI
+import SwiftUI
+import os
 
 final class GenerationDetailViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var mainRegion: Region?
+    @Published private var versionGroups = [VersionGroup]()
+    @Published private(set) var versions = [Version]()
+    @Published private(set) var types = [`Type`]()
     
+    @Published private(set) var details = [InfoKey: String]()
+    
+    @Published var showingMovesList = false
+    @Published var showingPokemonSpeciesList = false
+    
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "GenerationDetailViewModel")
+    
+    enum InfoKey: String, CaseIterable, Identifiable {
+        case abilities
+        case mainRegion = "main region"
+        case moves
+        case pokemonSpecies = "pokemon species"
+        case types
+        case versionGroups = "version groups"
+        
+        var title: LocalizedStringKey {
+            LocalizedStringKey(self.rawValue.localizedCapitalized)
+        }
+        
+        var id: Self { self }
+    }
+}
+
+extension GenerationDetailViewModel {
+    @MainActor
+    func loadData(generation: Generation, languageCode: String) async {
+        do {
+            self.mainRegion = try await Region(generation.mainRegion.url)
+            let versionGroups = try await Globals.getVersionGroups(from: generation.versionGroups.map { $0.url })
+            self.versionGroups.append(contentsOf: versionGroups)
+            let versions = try await Globals.getVersions(from: versionGroups).sorted()
+            self.versions.append(contentsOf: versions)
+            
+            let types = try await Globals.getTypes(urls: generation.types.map { $0.url }).sorted()
+            self.types.append(contentsOf: types)
+            self.details = getGenerationDetails(generation: generation, languageCode: languageCode)
+            viewLoadingState = .loaded
+        } catch {
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    func getGenerationDetails(generation: Generation, languageCode: String) -> [InfoKey: String] {
+        var details = [InfoKey: String]()
+        
+        details[.abilities] = "\(generation.abilities.count)"
+        if let mainRegion {
+            details[.mainRegion] = mainRegion.localizedName(languageCode: languageCode)
+        }
+        details[.moves] = "\(generation.moves.count)"
+        details[.pokemonSpecies] = "\(generation.pokemonSpecies.count)"
+        details[.types] = "\(generation.types.count)"
+        details[.versionGroups] = "\(generation.versionGroups.count)"
+        
+        return details
+    }
 }
--- PokedexRemake/Views/Detail views/GenerationDetail.swift
+++ PokedexRemake/Views/Detail views/GenerationDetail.swift
@@ -6,15 +6,100 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct GenerationDetail: View {
+    let generation: Generation
+    @StateObject private var viewModel = GenerationDetailViewModel()
+    @AppStorage(SettingsKey.language.rawValue) private var languageCode = SettingsKey.defaultLanguage
+    @StateObject private var movesListViewModel = MovesListViewModel()
+    @StateObject private var pokemonListViewModel = PokemonListViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(generation: generation, languageCode: languageCode)
+                }
+        case . loaded:
+            ScrollView {
+                Grid(alignment: .topLeading) {
+                    ForEach(GenerationDetailViewModel.InfoKey.allCases) { infoKey in
+                        GridRow {
+                            Text(infoKey.title)
+                                .foregroundColor(.gray)
+                            gridRowValue(for: infoKey)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(
+                viewModel.mainRegion != nil ?
+                viewModel.mainRegion!.localizedName(languageCode: languageCode) :
+                generation.mainRegion.name!
+            )
+            .sheet(isPresented: $viewModel.showingMovesList) {
+                MovesListView(
+                    title: viewModel.mainRegion!.localizedName(languageCode: languageCode),
+                    id: generation.id,
+                    description: "Moves in this generation",
+                    moveURLS: generation.moves.map { $0.url },
+                    viewModel: movesListViewModel
+                )
+                .presentationDetents([.large])
+                .presentationDragIndicator(.visible)
+            }
+            .sheet(isPresented: $viewModel.showingPokemonSpeciesList) {
+                Text("TODO")
+                .presentationDetents([.large])
+                .presentationDragIndicator(.visible)
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
+    }
+}
+
+private extension GenerationDetail {
+    @ViewBuilder
+    func gridRowValue(for infoKey: GenerationDetailViewModel.InfoKey) -> some View {
+        let value = viewModel.details[infoKey, default: "N/A"]
+        switch infoKey {
+        case .moves:
+            GridRowButton(items: generation.moves, value: value) {
+                viewModel.showingMovesList = true
+            }
+        case .pokemonSpecies:
+            GridRowButton(items: generation.pokemonSpecies, value: value) {
+                viewModel.showingPokemonSpeciesList = true
+            }
+        case .types:
+            LazyVGrid(columns: [.init(.adaptive(minimum: 100))], alignment: .leading, spacing: 5) {
+                if viewModel.types.isEmpty {
+                    Text("N/A")
+                } else {
+                    ForEach(viewModel.types) { type in
+                        TypeTag(type: type)
+                    }
+                }
+            }
+        case .versionGroups:
+            VStack(alignment: .leading) {
+                ForEach(viewModel.versions) { version in
+                    Text(version.localizedName(languageCode: languageCode))
+                }
+            }
+        default:
+            Text(value)
+        }
     }
 }
 
 struct GenerationDetail_Previews: PreviewProvider {
     static var previews: some View {
-        GenerationDetail()
+        NavigationStack {
+            GenerationDetail(generation: .example)
+        }
     }
 }

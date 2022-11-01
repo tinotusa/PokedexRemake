c4ba27fbf6af48fce63d23bdf9818433b643b76d
Tino Tusaumwe <tusatino@gmail.com>
2022-11-01 20:37:46 +1000

    Add GenerationDetailViewModel

---
+++ PokedexRemake/ViewModels/Details/GenerationDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  GenerationDetailViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 1/11/2022.
+//
+
+import Foundation
+
+final class GenerationDetailViewModel: ObservableObject {
+    
+}

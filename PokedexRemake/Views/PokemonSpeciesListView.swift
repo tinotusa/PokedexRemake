b869ac920a9f40dd4c50b0cd0f832a31699ada2b
Tino Tusaumwe <tusatino@gmail.com>
2022-11-02 19:29:37 +1000

    Add PokemonSpeciesListView

---
+++ PokedexRemake/Views/PokemonSpeciesListView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonSpeciesListView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 2/11/2022.
+//
+
+import SwiftUI
+
+struct PokemonSpeciesListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PokemonSpeciesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonSpeciesListView()
+    }
+}

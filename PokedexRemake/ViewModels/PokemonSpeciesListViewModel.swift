f216eee26f64f2fe4dc85ee0e3ca5e239b7a8a87
Tino Tusaumwe <tusatino@gmail.com>
2022-11-03 12:44:45 +1000

    Add PokemonSpeciesListUI

--- PokedexRemake/ViewModels/PokemonSpeciesListViewModel.swift
+++ PokedexRemake/ViewModels/PokemonSpeciesListViewModel.swift
@@ -6,7 +6,98 @@
 //
 
 import Foundation
+import SwiftPokeAPI
+import os
+
+struct PokemonGroup: Identifiable {
+    var id: Int { pokemon.id }
+    let pokemon: Pokemon
+    let pokemonSpecies: PokemonSpecies
+}
 
 final class PokemonSpeciesListViewModel: ObservableObject {
+    @Published private(set) var viewLoadingState = ViewLoadingState.loading
+    @Published private(set) var pokemonGroup = [PokemonGroup]() {
+        didSet {
+            if abs(pokemonGroup.count - oldValue.count) < limit {
+                hasNextPage = false
+            }
+        }
+    }
     
+    @Published private(set) var hasNextPage = true
+    private var isLoading = false
+    private var limit = 20
+    private var offset = 0
+    private var page = 0 {
+        didSet {
+             offset = page * limit
+        }
+    }
+    
+    private var urls = [URL]()
+    private var pokemonURLS = [URL]()
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "PokemonSpeciesListViewModel")
+}
+
+extension PokemonSpeciesListViewModel {
+    @MainActor
+    func loadData(speciesURL: [URL]) async {
+        do {
+            self.urls = speciesURL.sorted(by: pokemonSpeciesURLSort)
+            self.pokemonURLS = urls.map { URL(string: "https://pokeapi.co/api/v2/pokemon/\($0.lastPathComponent)")! }
+
+            let pokemonGroup = try await getPokemonSpecies().sorted { $0.pokemon.id < $1.pokemon.id }
+            self.pokemonGroup = pokemonGroup
+            
+            page += 1
+            viewLoadingState = .loaded
+        } catch {
+            viewLoadingState = .error(error: error)
+        }
+    }
+    
+    @MainActor
+    func getNextPage() async {
+        if !hasNextPage { return }
+        if isLoading { return }
+        isLoading = true
+        defer {
+            isLoading = false
+        }
+        do {
+            let pokemonGroup = try await getPokemonSpecies(offset: offset).sorted { $0.pokemon.id < $1.pokemon.id }
+            self.pokemonGroup.append(contentsOf: pokemonGroup)
+            page += 1
+        } catch {
+            logger.error("Failed to get next page. \(error)")
+        }
+    }
+}
+
+private extension PokemonSpeciesListViewModel {
+    func pokemonSpeciesURLSort(lhs: URL, rhs: URL) -> Bool {
+        if let id1 = Int(lhs.lastPathComponent),
+           let id2 = Int(rhs.lastPathComponent) {
+            return id1 < id2
+        }
+        return false
+    }
+
+    func getPokemonSpecies(limit: Int = 20, offset: Int = 0) async throws -> [PokemonGroup] {
+        try await withThrowingTaskGroup(of: PokemonGroup.self) { group in
+            for (i, _) in urls.enumerated() where i >= offset && i < offset + limit {
+                group.addTask {
+                    let pokemonSpecies = try await PokemonSpecies(self.urls[i])
+                    let pokemon = try await Pokemon(self.pokemonURLS[i])
+                    return PokemonGroup(pokemon: pokemon, pokemonSpecies: pokemonSpecies)
+                }
+            }
+            var results = [PokemonGroup]()
+            for try await item in group {
+                results.append(item)
+            }
+            return results
+        }
+    }
 }
--- PokedexRemake/Views/PokemonSpeciesListView.swift
+++ PokedexRemake/Views/PokemonSpeciesListView.swift
@@ -6,15 +6,72 @@
 //
 
 import SwiftUI
+import SwiftPokeAPI
 
 struct PokemonSpeciesListView: View {
+    let title: String
+    let speciesURLs: [URL]
+    @ObservedObject var viewModel: PokemonSpeciesListViewModel
+    @AppStorage(SettingsKey.language.rawValue) private var languageCode = SettingsKey.defaultLanguage
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text("Hello, World!")
+        switch viewModel.viewLoadingState {
+        case .loading:
+            ProgressView()
+                .task {
+                    await viewModel.loadData(speciesURL: speciesURLs)
+                }
+        case .loaded:
+            NavigationStack {
+                ScrollView {
+                    LazyVGrid(columns: [.init(.adaptive(minimum: 100))]) {
+                        ForEach(viewModel.pokemonGroup) { pokemonGroup in
+                            VStack(spacing: 0) {
+                                AsyncImage(url: pokemonGroup.pokemon.sprites.frontDefault) { image in
+                                    image
+                                        .interpolation(.none)
+                                        .resizable()
+                                        .scaledToFit()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: 100, height: 100)
+                                Text(pokemonGroup.pokemonSpecies.localizedName(languageCode: languageCode))
+                                    .padding()
+                            }
+                        }
+                        if viewModel.hasNextPage {
+                            ProgressView()
+                                .onAppear {
+                                    Task {
+                                        await viewModel.getNextPage()
+                                    }
+                                }
+                        }
+                    }
+                    .padding()
+                }
+                .bodyStyle()
+                .navigationTitle(title)
+                .toolbar {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+            }
+        case .error(let error):
+            ErrorView(text: error.localizedDescription)
+        }
     }
 }
 
 struct PokemonSpeciesListView_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonSpeciesListView()
+        PokemonSpeciesListView(
+            title: "Test title",
+            speciesURLs: Generation.example.pokemonSpecies.map { $0.url },
+            viewModel: PokemonSpeciesListViewModel()
+        )
     }
 }

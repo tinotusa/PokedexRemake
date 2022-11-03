cd3d285f2fab0f5375046c8ba9fb4a461c3ee87a
Tino Tusaumwe <tusatino@gmail.com>
2022-11-03 12:51:11 +1000

    Add AbilityListViewModel

---
+++ PokedexRemake/ViewModels/AbilityListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AbilityListViewModel.swift
+//  PokedexRemake
+//
+//  Created by Tino on 3/11/2022.
+//
+
+import Foundation
+import SwiftPokeAPI
+import os
+
+final class AbilityListViewModel: ObservableObject {
+    private let logger = Logger(subsystem: "com.tinotusa.PokedexRemake", category: "AbilityListViewModel")
+}

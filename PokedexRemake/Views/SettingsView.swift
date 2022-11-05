9362aedf9d019708cdcdcdae347dd945e670ce5a
Tino Tusaumwe <tusatino@gmail.com>
2022-11-05 21:13:34 +1000

    Add SettingsView

---
+++ PokedexRemake/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  PokedexRemake
+//
+//  Created by Tino on 5/11/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}

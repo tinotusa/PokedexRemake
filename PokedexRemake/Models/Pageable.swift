0d7af3c9cabe7f159a30595a3c014eb123982eaf
Tino Tusaumwe <tusatino@gmail.com>
2022-11-25 15:01:42 +1000

    Refactor Pageable protocol

--- PokedexRemake/Models/Pageable.swift
+++ PokedexRemake/Models/Pageable.swift
@@ -8,21 +8,13 @@
 import Foundation
 import SwiftPokeAPI
 
-// TODO: Update me
 /// A type that can be paginated.
 protocol Pageable {
-    associatedtype Value: Codable & SearchableByURL
-    
     /// The current pages information.
     var pageInfo: PageInfo { get }
     
-    /// The state of the page.
-    var pageState: PaginationState { get set }
-    
-    /// Fetches values based of the given page info.
-    /// - Parameter pageInfo: The current page info.
-    /// - Returns: A tuple of items and the information about the next page.
-    func loadPage(pageInfo: PageInfo) async throws -> (items: [Value], pageInfo: PageInfo)
+    /// Fetches some page info.
+    func loadPage() async
 }
 
 // TODO: Remove me
